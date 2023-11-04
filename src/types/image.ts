31c2ab46cc33f406805f6acf9016ba78269f1a00
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-11-04 10:10:28 +0700

    feat: Created converter from Blob/File to RGB Matrix.

--- src/types/image.ts
+++ src/types/image.ts
@@ -2,3 +2,5 @@ export interface ImageResult {
   image: File;
   similarity: number;
 }
+
+export type RGB = [number, number, number];
---
+++ src/types/matrix.ts
@@ -0,0 +1,5 @@
+import type { RGB } from "./image";
+
+export type Matrix<T> = T[][];
+
+export type RGBMatrix = Matrix<RGB>;
